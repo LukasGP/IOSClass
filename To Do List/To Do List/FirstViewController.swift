2717774e451d2f70ec747a830b35055faaa58a05
LukasGP <LukasPrusha@gmail.com>
2015-09-19 12:44:32 -0700

    started to do list app

---
+++ To Do List/To Do List/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  To Do List
+//
+//  Created by Lukas Prusha on 2015-09-19.
+//  Copyright (c) 2015 LukasGP. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ To Do List/To Do List/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  To Do List
+//
+//  Created by Lukas Prusha on 2015-09-19.
+//  Copyright (c) 2015 LukasGP. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
